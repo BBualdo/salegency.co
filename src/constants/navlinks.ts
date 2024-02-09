a71c15b0a95da1a4caaaaf854c5090c8b513e67a
Sebastian <mrseb10@gmail.com>
2024-02-09 02:05:52 +0100

    Navbar rework

---
+++ src/constants/navlinks.ts
@@ -0,0 +1,10 @@
+const navlinks = [
+  { href: "#mission", title: "Mission" },
+  { href: "#opinions", title: "Opinions" },
+  { href: "#offer", title: "Offer" },
+  { href: "#process", title: "Process" },
+  { href: "#results", title: "Results" },
+  { href: "#about", title: "About" },
+];
+
+export default navlinks;
