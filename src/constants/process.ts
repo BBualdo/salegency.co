17dfdf933c4d0742c22e1b90633d76aa37fe27f7
Sebastian <mrseb10@gmail.com>
2024-02-09 16:31:25 +0100

    Process template

---
+++ src/constants/process.ts
@@ -0,0 +1,36 @@
+const process = [
+  {
+    step: 1,
+    title: "Let’s talk",
+    description:
+      "Before signing the contract, our initial one or two calls focus on discussing essential elements, including necessary software, product structure, business model, and the implementation offer.",
+    timeline: "Up to 1 hour",
+    isUnfold: false,
+  },
+  {
+    step: 2,
+    title: "Sales Campaign Kickoff",
+    description:
+      "Swiftly executing discussed strategies, we adjust the product, business model, and offer, implementing required software. Post-execution, we plan and launch social media, ads, and email campaigns, aiming for tangible revenue results within the first 30 days.",
+    timeline: "0-30 days",
+    isUnfold: false,
+  },
+  {
+    step: 3,
+    title: "Enhancing Post-Campaign",
+    description:
+      "Post-sales campaign, we collaborate to enhance your product for an attractive offer and an outstanding user experience. The goal is to generate monthly revenue from both your existing audience and new customers within 30-60 days.",
+    timeline: "30-60 days",
+    isUnfold: false,
+  },
+  {
+    step: 4,
+    title: "Customer Growth and Optimization",
+    description:
+      "As new customers are acquired, we ensure their retention, referrals, and progression through continuous sales process management and optimization. This approach not only enhances monetization but also strengthens your brand.",
+    timeline: "60 days+",
+    isUnfold: false,
+  },
+];
+
+export default process;
