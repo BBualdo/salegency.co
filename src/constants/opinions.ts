e7154c02c5110bce73adf98971a8def791d2ea01
Sebastian <mrseb10@gmail.com>
2024-02-09 11:16:52 +0100

    Opinions template

---
+++ src/constants/opinions.ts
@@ -0,0 +1,39 @@
+const opinions = [
+  {
+    image: "/img/Charro-Pyl.avif",
+    author: "Charro Pyl",
+    company: "FitByCharro",
+    content:
+      "In just 12 months, I went from generating a meager income of a few thousand euros to earning a substantial 750,000 euros in revenue.",
+  },
+  {
+    image: "/img/Kamil-Kuc.avif",
+    author: "Kamil Kuc",
+    company: "Orety",
+    content:
+      "One of the best business decisions I've made in recent years. Together, we created several websites and two digital products, which doubled my company's turnover.",
+  },
+  {
+    image: "/img/Loran-Simon.avif",
+    author: "Loran Simon",
+    company: "Somnowell",
+    content:
+      "In the first month of going live, we increased revenue by 300% compared to the previous month. What can I say? Highly recommended.",
+  },
+  {
+    image: "/img/Nathan-Huges.avif",
+    author: "Nathan Huges",
+    company: "Matt Diggity",
+    content:
+      "We witnessed a dramatic increase in email engagement across multiple email lists, and deliverability improved across the board.",
+  },
+  {
+    image: "/img/Piotr-Blicharski.avif",
+    author: "Piotr Blicharski",
+    company: "2S Design",
+    content:
+      "We smoothly transitioned the strategy into tangible results, leading to impressive sales outcomes in a short time.",
+  },
+];
+
+export default opinions
