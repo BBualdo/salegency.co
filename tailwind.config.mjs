5d37a1b54c2681a5f692439bc9e44fa23dde6d59
Sebastian <mrseb10@gmail.com>
2024-02-16 14:06:23 +0100

    RWD:Offer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-			"greencell": "#7ED957",
-			"lime": "#d9fc67",
-			},
-			screens: {
-				"lg": "1104px",
-				"xs": "200px"
-			}
-		},
-		
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        greencell: "#7ED957",
+        lime: "#d9fc67",
+      },
+      screens: {
+        lg: "1104px",
+        xs: "200px",
+      },
+    },
+  },
+  plugins: [],
+};
